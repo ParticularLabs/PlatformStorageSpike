bb039f14464719ea36fd0ce5e1a22fe782a6cfe1
Tomek Masternak <tomek.masternak@gmail.com>
2022-02-03 10:28:25 +0100

    project scafolding

---
+++ PlatformStorageSpike/PlatformStorageSpike.Ingestor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlatformStorageSpike.Ingestor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
