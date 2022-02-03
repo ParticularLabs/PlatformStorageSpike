ff7aba94c2410130940458df1d3f82f54f9918b8
Tomek Masternak <tomek.masternak@gmail.com>
2022-02-03 10:51:26 +0100

    write a simple blob

--- PlatformStorageSpike/PlatformStorageSpike.Ingestor/Program.cs
+++ PlatformStorageSpike/PlatformStorageSpike.Ingestor/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 
 namespace PlatformStorageSpike.Ingestor
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task  Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var blobClient = new BlobServiceClient(Environment.GetEnvironmentVariable("PlatformSpike_BlobContainerConnectionString"));
+
+            var blobContainerClient = blobClient.GetBlobContainerClient("platform-spike-storage");
+            await blobContainerClient.CreateIfNotExistsAsync().ConfigureAwait(false);
+
+            var blob = blobContainerClient.GetBlobClient("test");
+
+            var options = new BlobUploadOptions
+            {
+                Metadata = new Dictionary<string, string>()
+                {
+                    { "ContentType", "content-type" },
+                    { "BodySize", "123"}
+                }
+            };
+
+            await blob.UploadAsync(
+                BinaryData.FromBytes(new byte[0]),
+                options,
+                CancellationToken.None
+            ).ConfigureAwait(false);
         }
     }
 }
