2c1b5cdddd7c31f62607582e5039eb59880c6580
Tomek Masternak <tomek.masternak@gmail.com>
2022-02-03 11:58:47 +0100

    some sample data

--- PlatformStorageSpike/PlatformStorageSpike.Ingestor/Program.cs
+++ PlatformStorageSpike/PlatformStorageSpike.Ingestor/Program.cs
@@ -11,26 +11,46 @@ namespace PlatformStorageSpike.Ingestor
     {
         static async Task  Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //HINT: Body and headers represent immutable information that do not change once defined. 
+            //      Metadata represents information that is specific to a concrete message handling.
 
+            var messageIdHeaderName = "NServiceBus.MessageId";
+
+            var body = new byte[5*1024]; //KB message body
+            var headers = new Dictionary<string, string>
+            {
+                {messageIdHeaderName, "7d6bce1d-829b-4fba-abc6-ab2900b53718"},
+                {"NServiceBus.ContentType", "text/xml"},
+                {"NServiceBus.ConversationId", "b613bb02-a97e-4298-a9e9-ab2900b53723"},
+                {"NServiceBus.CorrelationId", "7d6bce1d-829b-4fba-abc6-ab2900b53718"},
+                {"NServiceBus.EnclosedMessageTypes", "Core7.Headers.Writers.MyNamespace.MessageToSend, MyAssembly, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"},
+                {"NServiceBus.MessageIntent", "Send"},
+                {"NServiceBus.OriginatingEndpoint", "HeaderWriterAuditV7"},
+                {"NServiceBus.OriginatingMachine", "MACHINENAME"},
+                {"NServiceBus.ReplyToAddress", "HeaderWriterAuditV7"},
+                {"NServiceBus.TimeSent", "2019-12-20 10:59:47:141171 Z"},
+                {"NServiceBus.Version", "7.2.0"},
+            };
+
+            //Data that we or customer associate with a message that failed during processing 
+            var metadata = @"";
+
+
+            //codify the update commit header on the blob
             var blobClient = new BlobServiceClient(Environment.GetEnvironmentVariable("PlatformSpike_BlobContainerConnectionString"));
 
             var blobContainerClient = blobClient.GetBlobContainerClient("platform-spike-storage");
             await blobContainerClient.CreateIfNotExistsAsync().ConfigureAwait(false);
 
-            var blob = blobContainerClient.GetBlobClient("test");
+            var blob = blobContainerClient.GetBlobClient(headers[messageIdHeaderName]);
 
             var options = new BlobUploadOptions
             {
-                Metadata = new Dictionary<string, string>()
-                {
-                    { "ContentType", "content-type" },
-                    { "BodySize", "123"}
-                }
+                Metadata = headers
             };
 
             await blob.UploadAsync(
-                BinaryData.FromBytes(new byte[0]),
+                BinaryData.FromBytes(body),
                 options,
                 CancellationToken.None
             ).ConfigureAwait(false);
