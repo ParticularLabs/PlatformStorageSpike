4f543475f54e2ad60ca787033fc4118fe5321301
Andreas Öhlund <andreas.ohlund@particular.net>
2022-02-04 10:54:30 +0100

    Codify ingestion

--- PlatformStorageSpike/PlatformStorageSpike.Ingestor/Program.cs
+++ PlatformStorageSpike/PlatformStorageSpike.Ingestor/Program.cs
@@ -13,10 +13,15 @@ namespace PlatformStorageSpike.Ingestor
         {
             //HINT: Body and headers represent immutable information that do not change once defined. 
             //      Metadata represents information that is specific to a concrete message handling.
+            var blobClient = new BlobServiceClient(Environment.GetEnvironmentVariable("PlatformSpike_BlobContainerConnectionString"));
+
+            var blobContainerClient = blobClient.GetBlobContainerClient("platform-spike-storage");
+            await blobContainerClient.CreateIfNotExistsAsync().ConfigureAwait(false);
+
 
             var messageIdHeaderName = "NServiceBus.MessageId";
 
-            var body = new byte[5*1024]; //KB message body
+            var body = new ReadOnlyMemory<byte>(new byte[5*1024]); //KB message body
             var headers = new Dictionary<string, string>
             {
                 {messageIdHeaderName, "7d6bce1d-829b-4fba-abc6-ab2900b53718"},
@@ -32,28 +37,70 @@ namespace PlatformStorageSpike.Ingestor
                 {"NServiceBus.Version", "7.2.0"},
             };
 
-            //Data that we or customer associate with a message that failed during processing 
-            var metadata = @"";
+            var messageId = headers[messageIdHeaderName];
+
+            await StoreBodyAndHeaders(blobContainerClient, messageId, body, headers);
+
+            var processedAt = DateTimeOffset.UtcNow.ToString();
+            var processingEndpoint = "MyProcessingEndpoint";
+            var processingAttemptId = $"{messageId}-{processingEndpoint}-{processedAt}";
+
+            var processingMetadata = new Dictionary<string, string> 
+            {
+                { "ProcessingAttemptId", processingAttemptId },
+                { "ProcessedAt", processedAt },
+                { "ProcessingEndpoint", processingEndpoint}
+            };
 
 
+            var isConfirmed = false; // isConfirmed == TxLevel < AtomicSendsWithReceive
+
+            await StoreProcessingMetadata(blobContainerClient, processingAttemptId, processingMetadata, isConfirmed);
+
+            await MarkProcessingMetadataAsConfirmed(blobContainerClient, processingAttemptId);
+
+        }
+
+        static Task MarkProcessingMetadataAsConfirmed(BlobContainerClient blobContainerClient, string processingAttemptId)
+        {
+            throw new NotImplementedException();
+        }
+
+        static Task StoreBodyAndHeaders(BlobContainerClient blobContainerClient, string messageId, ReadOnlyMemory<byte> body, IDictionary<string, string> headers)
+        {
             //codify the update commit header on the blob
-            var blobClient = new BlobServiceClient(Environment.GetEnvironmentVariable("PlatformSpike_BlobContainerConnectionString"));
-
-            var blobContainerClient = blobClient.GetBlobContainerClient("platform-spike-storage");
-            await blobContainerClient.CreateIfNotExistsAsync().ConfigureAwait(false);
-
-            var blob = blobContainerClient.GetBlobClient(headers[messageIdHeaderName]);
+            var blob = blobContainerClient.GetBlobClient(messageId);
 
             var options = new BlobUploadOptions
             {
                 Metadata = headers
             };
 
-            await blob.UploadAsync(
+            //TODO: store headers in a separate blob if size is above 8KB
+
+            return blob.UploadAsync(
                 BinaryData.FromBytes(body),
                 options,
-                CancellationToken.None
-            ).ConfigureAwait(false);
+                CancellationToken.None);
+        }
+
+        static Task StoreProcessingMetadata(
+            BlobContainerClient blobContainerClient, 
+            string processingMetadataId, 
+            IDictionary<string, string> processingMetadata,
+            bool isConfirmed)
+        {
+            var blob = blobContainerClient.GetBlobClient(processingMetadataId);  //TODO: do we need a prefix?
+
+            var options = new BlobUploadOptions
+            {
+                Metadata = new Dictionary<string, string> { { "IsConfirmed", isConfirmed.ToString() } }
+            };
+
+            return blob.UploadAsync(
+                BinaryData.FromBytes(System.Text.Json.JsonSerializer.SerializeToUtf8Bytes(processingMetadata)),
+                options,
+                CancellationToken.None);
         }
     }
 }
