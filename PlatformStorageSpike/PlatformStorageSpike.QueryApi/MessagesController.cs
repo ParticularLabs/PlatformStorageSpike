c2c3d6edcd1477cdfb30118403fead5123e64536
Tomek Masternak <tomek.masternak@gmail.com>
2022-02-07 18:28:43 +0100

    dapper and some sorting for messages

--- PlatformStorageSpike/PlatformStorageSpike.QueryApi/MessagesController.cs
+++ PlatformStorageSpike/PlatformStorageSpike.QueryApi/MessagesController.cs
@@ -1,4 +1,6 @@
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using ServiceControl.CompositeViews.Messages;
 using ServiceControl.Contracts.Operations;
 
@@ -6,8 +8,38 @@ public class MessagesController : ControllerBase
 {
     [HttpGet]
     [Route("/api/messages")]
-    public List<MessagesView> Get()
+    public List<MessagesView> Get(int per_page=10, int page=0, string direction = "asc", bool includeSystemMessages = false, string sort = "time_sent")
     {
+        var connectionString = Environment.GetEnvironmentVariable("PlatformSpike_AzureSQLConnectionString");
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+            var sortColumn = new Dictionary<string, string>
+            {
+                {"time_sent", "TimeSent"},
+                {"message_type", "MessageType"},
+                {"processing_time", "ProcessingTimeMs"},
+                {"id", "MessageId"}
+            };
+            var pageQuery = @$"SELECT * FROM Messages ORDER BY {sortColumn[sort]} {direction} OFFSET @Offset ROWS FETCH NEXT @PageSize ROWS ONLY";
+            var messages = connection.Query<MessagesView>(
+                pageQuery, 
+                new
+                {
+                    Offset = (page-1)*per_page, 
+                    PageSize = per_page,
+                    Direction = direction
+                }
+                ).ToList();
+
+            var totalMessages = connection.Query<int>("SELECT count(*) FROM Messages").First();
+
+            Response.Headers.Add("Total-Count", totalMessages.ToString());
+
+            return messages;
+        }
+
+        /*
         var messages = new List<MessagesView>
         {
             new (){ MessageId = "1", MessageType = "Some.Type", ProcessingTime = TimeSpan.FromSeconds(1), TimeSent = DateTime.Now, Status = MessageStatus.Successful },
@@ -15,9 +47,6 @@ public class MessagesController : ControllerBase
             new (){ MessageId = "3", MessageType = "Some.Type", ProcessingTime = TimeSpan.FromSeconds(1), TimeSent = DateTime.Now, Status = MessageStatus.Successful },
             new (){ MessageId = "4", MessageType = "Some.Type", ProcessingTime = TimeSpan.FromSeconds(1), TimeSent = DateTime.Now, Status = MessageStatus.Successful }
         };
-
-        Response.Headers.Add("Total-Count", messages.Count.ToString());
-
-        return messages;
+        */
     }
 }
